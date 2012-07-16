2fea9eb7ad439ddb597cf469684c935872c9cf8e
Tomas Lin <tomaslin@gmail.com>
2012-07-17 00:23:10 +0100

    started using betamax to record interactions

--- grails-app/services/com/kickscraper/ScrapeService.groovy
+++ grails-app/services/com/kickscraper/ScrapeService.groovy
@@ -5,6 +5,7 @@ import org.codehaus.groovy.grails.plugins.codecs.URLCodec
 
 class ScrapeService {
 
+    static browser = new Browser()
 
     def search(term) {
         processSearchPage "http://www.kickstarter.com/projects/search?term=${ URLCodec.encode(term) }"
@@ -15,8 +16,7 @@ class ScrapeService {
     }
 
     private def processRewardsPage(url) {
-
-        Browser.drive {
+        browser.drive {
             go url
             $('.NS-projects-reward.clickable').each { element ->
                 println element.find('h3').text()
@@ -25,14 +25,14 @@ class ScrapeService {
                 println element.find('.delivery-date').text()
                 println element.find('.desc').text()
             }
-
         }
-
     }
 
     private def processSearchPage(url) {
 
-        Browser.drive {
+        def items = []
+
+        browser.drive {
 
             go url
 
@@ -41,22 +41,26 @@ class ScrapeService {
                 def date = element.find('li.ksr_page_timer').@'data-end_time'
 
                 if (date) {
-                    def title = element.find('h2')
-                    println title.find('a').first().@href
-                    println title.find('a').text()
-                    println date
+
+                    def item = [:]
+                    def titleElement = element.find('h2')
+                    item.link = titleElement.find('a').first().@href
+                    item.title = titleElement.find('a').text()
+                    item.date = date
+                    items << item
                 }
             }
 
             if ($('div.pagination') && !url.contains('page=')) {
-                def numberOfPages = $('div.pagination a')[-2].@href.split(
-                        'page=')[1].split('&')[0]
+                def numberOfPages = $('div.pagination a')[-2].@href.split('page=')[1].split('&')[0]
 
                 (2..(numberOfPages as int)).each {
-                    processSearchPage url.replace('term=', 'page=' + it + '&term=')
+                    items += processSearchPage url.replace('term=', 'page=' + it + '&term=')
                 }
             }
         }
+
+        items
     }
 
 }
--- test/unit/com/kickscraper/ScrapeServiceSpec.groovy
+++ test/unit/com/kickscraper/ScrapeServiceSpec.groovy
@@ -5,15 +5,23 @@ import betamax.Recorder
 import org.junit.Rule
 import betamax.Betamax
 import grails.test.mixin.TestFor
+import com.gargoylesoftware.htmlunit.ProxyConfig
 
 @TestFor(ScrapeService)
 class ScrapeServiceSpec extends Specification{
 
     @Rule Recorder recorder = new Recorder()
 
+    def setup(){
+        def proxyConfig = new ProxyConfig("localhost", 5555)
+        proxyConfig.addHostsToProxyBypass("localhost")
+        service.browser.driver.webClient.proxyConfig = proxyConfig
+    }
+
     @Betamax(tape='search')
     def "search returns correct information"(){
-
+        expect:
+        service.search('ipad').size() == 32
     }
 
     @Betamax(tape='minidrive')
